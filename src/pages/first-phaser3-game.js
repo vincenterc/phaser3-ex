24a54c56ead0b6d1ee5479f19055b2fa4a22fa97
vincenterr <chung.menghsun@gmail.com>
2018-12-31 09:37:28 +0800

    Add the keyboard control to the player

--- src/pages/first-phaser3-game.js
+++ src/pages/first-phaser3-game.js
@@ -42,6 +42,7 @@ class FirstPhaser3GamePage extends React.Component {
 
     let platforms
     let player
+    let cursors
 
     function create() {
       this.add.image(400, 300, 'sky')
@@ -59,10 +60,45 @@ class FirstPhaser3GamePage extends React.Component {
       player.setBounce(0.2)
       player.setCollideWorldBounds(true)
 
+      this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1,
+      })
+      this.anims.create({
+        key: 'turn',
+        frames: [{ key: 'dude', frame: 4 }],
+        frameRate: 20,
+      })
+      this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1,
+      })
+
+      cursors = this.input.keyboard.createCursorKeys()
+
       this.physics.add.collider(player, platforms)
     }
 
-    function update() {}
+    function update() {
+      if (cursors.left.isDown) {
+        player.setVelocityX(-160)
+        player.anims.play('left', true)
+      } else if (cursors.right.isDown) {
+        player.setVelocityX(160)
+        player.anims.play('right', true)
+      } else {
+        player.setVelocityX(0)
+        player.anims.play('turn')
+      }
+
+      if (cursors.up.isDown && player.body.touching.down) {
+        player.setVelocityY(-330)
+      }
+    }
   }
 
   render() {
